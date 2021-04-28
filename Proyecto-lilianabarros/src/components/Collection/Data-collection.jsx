ee059bd07faf751b07949c714c5330a8d1b47ccc
LilianaBarros <lilianabarrosc@gmail.com>
2021-04-28 01:10:19 -0400

    se agrega link a card

--- Proyecto-lilianabarros/src/components/Collection/Data-collection.jsx
+++ Proyecto-lilianabarros/src/components/Collection/Data-collection.jsx
@@ -9,8 +9,8 @@ import Typography from "@material-ui/core/Typography";
 import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
-import { imageUrl } from '../../constants';
-import PropTypes from 'prop-types';
+import { imageUrl } from "../../constants";
+import PropTypes from "prop-types";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -43,57 +43,68 @@ const DataCollection = ({
   const youHandleClickFunc = () => {};
 
   return (
-    <Card className={classes.root} onClick={() => youHandleClickFunc()}>
-      <CardActionArea>
-        <CardContent>
-          <Typography>
-            <div className={classes.cover}>
-              <img
-                className={classes.cover}
-                src={`${imageUrl}${data_poster_path} `}
-                alt="title"
-              />
-            </div>
-          </Typography>
-          <Typography gutterBottom variant="h6" component="span">
-            {data_title}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {data_release_date}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Box position="relative" display="inline-flex">
-          <CircularProgress className={data_vote_average > 7 ? 'MuiCircularProgress-colorPrimary' : 'MuiCircularProgress-colorSecondary'} variant="static" value={data_vote_average * 10} />
-          <Box
-            top={0}
-            left={0}
-            bottom={0}
-            right={0}
-            position="absolute"
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-          >
-            <Typography
-              variant="caption"
-              component="div"
-              color="textSecondary"
-            >{`${data_vote_average * 10}%`}</Typography>
-          </Box>
-        </Box>
 
-        <Button
-          size="small"
-          color="primary"
-          component={Link}
-          to={`${data_link_detail}${data_id}`}
-        >
-          Ver Detalle
-        </Button>
-      </CardActions>
-    </Card>
+      <Card className={classes.root} onClick={() => youHandleClickFunc()}>
+        <CardActionArea component={Link}
+            to={`${data_link_detail}${data_id}`}>
+          <CardContent>
+            <Typography>
+              <div className={classes.cover}>
+                <img
+                  className={classes.cover}
+                  src={`${imageUrl}${data_poster_path} `}
+                  alt="title"
+                />
+              </div>
+            </Typography>
+            <Typography gutterBottom variant="h6" component="span">
+              {data_title}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {data_release_date}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Box position="relative" display="inline-flex">
+            <CircularProgress
+              className={
+                data_vote_average > 7
+                  ? "MuiCircularProgress-colorPrimary"
+                  : "MuiCircularProgress-colorSecondary"
+              }
+              variant="static"
+              value={data_vote_average * 10}
+            />
+            <Box
+              top={0}
+              left={0}
+              bottom={0}
+              right={0}
+              position="absolute"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Typography
+                variant="caption"
+                component="div"
+                color="textSecondary"
+              >{`${data_vote_average * 10}%`}</Typography>
+            </Box>
+          </Box>
+
+          <Button
+            size="small"
+            color="primary"
+            component={Link}
+            to={`${data_link_detail}${data_id}`}
+          >
+            Ver Detalle
+          </Button>
+        </CardActions>
+      </Card>
+
   );
 };
 
@@ -106,6 +117,6 @@ DataCollection.propTypes = {
   data_release_date: PropTypes.string,
   data_poster_path: PropTypes.string,
   data_link_detail: PropTypes.string,
-}
+};
 
 export default DataCollection;
